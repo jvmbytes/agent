e4c73aaf798648fe8636787a2d190030f50150dc
wongoo <wongoo@apache.org>
2020-05-29 17:36:37 +0800

    init

---
+++ src/main/java/com/jvmbytes/spy/agent/SpyAgent.java
@@ -0,0 +1,15 @@
+package com.jvmbytes.spy.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author wongoo
+ */
+public class SpyAgent {
+
+    public static void premain(String featureString, Instrumentation inst) {
+    }
+
+    public static void agentmain(String featureString, Instrumentation inst) {
+    }
+}
